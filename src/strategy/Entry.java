64cfe16bd8869e5ed4b5f43cd3f7af89377aa4eb
Max <maksima7th@gmail.com>
2024-03-04 22:35:44 +0700

    commit 6

---
+++ src/strategy/Entry.java
@@ -0,0 +1,46 @@
+package src.strategy;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Entry implements Serializable {
+    Long key;
+    String value;
+    Entry next;
+    int hash;
+
+    public Entry(int hash, Long key, String value, Entry next) {
+        this.hash = hash;
+        this.key = key;
+        this.value = value;
+        this.next = next;
+    }
+
+    public final Long getKey() {
+        return key;
+    }
+
+    public final String getValue() {
+        return value;
+    }
+
+    public final int hashCode() {
+        return Objects.hashCode(getKey()) ^ Objects.hashCode(getValue());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Entry entry = (Entry) o;
+        if (key != null ? !key.equals(entry.key) : entry.key != null) return false;
+        return value != null ? value.equals(entry.value) : entry.value == null;
+
+
+    }
+
+    public final String toString() {
+        return getKey() + "=" + getValue();
+    }
+}
